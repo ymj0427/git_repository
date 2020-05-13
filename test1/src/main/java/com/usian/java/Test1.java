776c7289d9bcdbc52f9f6d472ecead6c0c2101f6
ymj0427 <65162110+ymj0427@users.noreply.github.com>
2020-05-13 15:02:57 +0800

    Update Test1.java

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -1,7 +1,7 @@
 package com.usian.java;
 
 public class Test1 {
-    //更新Test1
+    //更新Test1    远程仓库：快快下载我
     public static void main(String[] args) {
         System.out.println("李林？？？？？？奥利奥利奥利给！！！");
     }
