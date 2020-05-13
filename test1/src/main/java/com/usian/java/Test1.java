f56369ef6f7650c5abc7e8e38cc5975cd66fbe28
ymj0427 <65162110+ymj0427@users.noreply.github.com>
2020-05-13 15:13:27 +0800

    Update Test1.java

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -1,8 +1,7 @@
 package com.usian.java;
-
+//这是我远程仓库的地盘
 public class Test1 {
     //更新Test1    远程仓库：快快下载我
-    //我本地仓库就是要抢你的地盘！！！
     public static void main(String[] args) {
         System.out.println("李林？？？？？？奥利奥利奥利给！！！");
     }
