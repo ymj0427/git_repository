0308ed340158a9760487eb807567c4aecf3238ee
ymj0427 <zl0427ymj>
2020-05-13 15:05:11 +0800

    更新Test1

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -2,6 +2,7 @@ package com.usian.java;
 
 public class Test1 {
     //更新Test1    远程仓库：快快下载我
+    //我本地仓库就是要提交抢你地盘！！！
     public static void main(String[] args) {
         System.out.println("李林？？？？？？奥利奥利奥利给！！！");
     }
